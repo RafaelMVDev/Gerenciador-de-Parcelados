3a2592307d7ce67d154a217ab77b4d4653ac6c20
RafaelMVDev <rafaelmoralesvillela@gmail.com>
2025-02-13 21:51:56 -0300

    forms em /login

--- login/urls.py
+++ login/urls.py
@@ -3,6 +3,8 @@ from django.urls import path,include
 
 from . import views
 
+app_name = 'login'
 urlpatterns = [
-    path('',views.login,name = 'login')
+    path('',views.login,name = 'login'),
+    path('request',views.request,name ='request')
 ]
--- login/views.py
+++ login/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def login(response):
     return render(response,'login\login.html')
+
+
+def request(response):
+    if response.method == "POST":
+        print("CHEGOU EM FORMATO DE POST...")
+        return '<html><head> </head> <body> </body></html>'
+    else:
+        return '<html><head> </head> <body> </body></html>'
